48edfb003814e50ae01a72fa3ba1306831681639
Khromushkin Nikita <n.khromushkin@qiwi.com>
2020-06-18 12:37:01 +0300

    add payout

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const qiwiPublicKey = process.env.QIWI_PUBLIC_KEY;
 const qiwiEdgeToken = process.env.QIWI_EDGE_TOKEN;
 const mongoUrl = process.env.MONGODB_URI;
 const ratesUrl = 'https://edge.qiwi.com/sinap/crossRates';
+const pauoutCardUrl = 'https://edge.qiwi.com/sinap/api/v2/terms/21013/payments';
+const pauoutWalletUrl = 'https://edge.qiwi.com/sinap/api/v2/terms/99/payments';
+const comissionCardUrl = 'https://edge.qiwi.com/sinap/providers/21013/onlineCommission';
+const comissionWalletUrl = 'https://edge.qiwi.com/sinap/providers/99/onlineCommission';
+const payoutCardPan = process.env.PAYOUT_CARD_PAN;
+const payoutWallet = process.env.PAYOUT_WALLET;
 
 async function startApp() {
     console.log('start app begin')
@@ -33,6 +39,8 @@ async function startApp() {
         .post('/qiwi-notification', qiwiNotificationHandler)
         .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+    await payout('7907e7f5-c55f-44e5-8622-3f3b18ae1ceb');
+
     async function checkoutHandler(req, res) {
         const invoiceParams = req.body;
         const billId = qiwiApi.generateId();
@@ -48,8 +56,6 @@ async function startApp() {
             expirationDateTime: moment().add(1, 'day').toISOString(true)
         })
         await customersCollection.updateOne({_id: billId}, {$set: qiwiBill}, {upsert: true});
-        // const res = await customersCollection.findOne({_id: mongodb.ObjectId(insereRes.insertedId.toString())});
-
         res.send(JSON.stringify(qiwiBill));
     }
 
@@ -58,8 +64,9 @@ async function startApp() {
         if (!qiwiApi.checkNotificationSignature(req.headers['x-api-signature-sha256'], req.body, qiwiSecretKey)) {
             throw new Error('WRONG_SIGNATURE')
         }
-        console.log('got bill notification', req.body.bill.billId);
+        console.log('got bill notification', req.body.bill);
         await customersCollection.updateOne({_id: req.body.bill.billId}, {$set: req.body.bill}, {upsert: true});
+        await payout(req.body.bill.billId);
         return res.status(204).send();
     }
 
@@ -71,6 +78,73 @@ async function startApp() {
         });
         const rates = await currencyRateResponse.json();
         return usdAmount / rates.result.find(rate => (rate.from === '840' && rate.to === '643')).rate;
+    }
+
+    async function payout(billId) {
+        const storedBill = await customersCollection.findOne({_id: billId});
+        if (!storedBill)
+            return;
+        if (storedBill.status.value !== 'PAID') {
+            return;
+        }
+        if (storedBill.payoutResult && storedBill.payoutResult.transaction.state.code === 'Accepted') {
+            return;
+        }
+        console.log('start payout', billId);
+        const comissionResponse = await fetch(comissionWalletUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + qiwiEdgeToken
+            },
+            body: JSON.stringify({
+                account: payoutWallet,
+                paymentMethod: {
+                    type: 'Account',
+                    accountId: '643'
+                },
+                purchaseTotals: {
+                    total: {
+                        amount: Number(storedBill.amount.value),
+                        currency: '643'
+                    }
+                }
+            })
+        });
+        const comission = await comissionResponse.json();
+        console.log('got commission for amount', storedBill.amount.value, comission.qwCommission.amount);
+        const payoutAmount = Number(storedBill.amount.value) - comission.qwCommission.amount;
+        const payoutResponse = await fetch(pauoutWalletUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + qiwiEdgeToken
+            },
+            body: JSON.stringify({
+                id: (moment().unix() * 1000).toString(),
+                account: payoutWallet,
+                paymentMethod: {
+                    type: 'Account',
+                    accountId: '643'
+                },
+                sum: {
+                    amount: payoutAmount,
+                    currency: '643'
+                },
+                fields: {
+                    account: '+' + payoutWallet
+                }
+            })
+        });
+        const payoutResult = await payoutResponse.json();
+        console.log('got payout result for bill', billId, payoutResult.transaction.state.code);
+        if (payoutResult && payoutResult.transaction.state.code === 'Accepted') {
+            storedBill.payoutResult = payoutResult;
+            await customersCollection.updateOne({_id: billId}, {$set: storedBill}, {upsert: true});
+            console.log('bill payout completed', billId, payoutResult.transaction);
+        }
     }
 }
 
